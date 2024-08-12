63e4397482d0c1afc19c064a966f5321047f12ce
Jochen Kall (CR/ADI2.1) <jochen.kall@de.bosch.com>
2024-08-12 16:07:49 +0200

    Add Modifier Stub for humgen v4 face modification
    Signed-off-by: Jochen Kall (CR/ADI2.1) <jochen.kall@de.bosch.com>

--- src/anyhuman/ops.py
+++ src/anyhuman/ops.py
@@ -606,3 +606,27 @@ def ReplaceMaterials(obj, args, sMode, **kwargs):
 
 
 # enddef
+def RandomizeFace(Collection, args, sMode, **kwargs):
+    """
+    Randomize the faces of all humgen v4 models in the collection.
+    Only for humgen v4 humans with face controls
+
+    Modifier arguments:
+        fRandomizationStrength: float in [0,1] for the strength of the randomization, i.e.
+            how extreme the facial expressions are supposed to be
+        iSeed: seed for randomization
+
+    Parameters
+    ----------
+    Collection : Blender Collection containing the humans to be modified
+    args : dict
+        Dictionary with configuration arguments
+    """
+
+    # Extract modifier parameters
+    fRandomizationStrength = convert.DictElementToFloat(args, "fRandomizationStrength", fDefault=0.4)
+    iSeed = convert.DictElementToInt(args, "iSeed", iDefault=0)
+
+    print("I bins, de Batman!!!")
+    print(fRandomizationStrength)
+    print(iSeed)
